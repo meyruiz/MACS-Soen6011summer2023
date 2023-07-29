a9f8367bd475382e0ebcc370847db83e063243b0
Mayra Ruiz <mey.ruiz27@gmail.com>
2023-07-29 15:38:27 -0400

    #117 Fix bug when loading all jobs a candidate has applied to

--- fe-career-platform/src/pages/Home/Home-Content/index.js
+++ fe-career-platform/src/pages/Home/Home-Content/index.js
@@ -1,113 +1,104 @@
 import { Button, Card, CardActions, CardContent, Link, Typography } from '@mui/material';
 import React, { Component, useEffect, useState } from 'react'
 import ApiFun from '../../../Service/api';
+import Navbar from '../../Home/Navbar/navbar'
 
-export default function HomeContent() {
+export default function CandidateApplications() {
     const [isStudent, setIsStudent] = useState(false);
     const [allAvailableJobs, setallAvailableJobs] = useState([])
-    useEffect(() => {
-        const candidateId = localStorage.getItem("userid");
-        // console.log(loggedInUserID);
+    const [allJobsFetched, setAllJobsFetched] = useState(false);
 
-        // check if the user is Candidate
+    // Fetch all available jobs when the page loads
+    useEffect(() => {
         const role = localStorage.getItem('userRole')
-        // console.log(role.toLowerCase())
         if(role && role.toLowerCase()  === 'candidate') {
-            // console.log(role)
             setIsStudent(true)
         }
 
-        // render all available jobs
         ApiFun.getApi("/jobs/all")
             .then((res=> {
-                // console.log(res.data);
-                setallAvailableJobs([...allAvailableJobs, ...res.data])
+                setallAvailableJobs(res.data)
+                setAllJobsFetched(true);
             }))
             .catch((err) => {
                 console.error(err)
             });
-        
-        const tempList = [];
-        if(candidateId && isStudent) {
-            // get the all applied jobs
+    }, [])
+
+    useEffect(() => {
+        if(isStudent && allJobsFetched) {
+            const candidateId = localStorage.getItem("userid");
             ApiFun.getApi(`/candidate/${candidateId}/jobs`).then((e) => {
                 console.log(e)
-                // e.data.map((data) => {
-                //     console.log(data._id)
-                //     tempList.push(data._id)
-                // })
+                const appliedJobIds = e.data.map(job => job._id);
+                const updatedJobs = allAvailableJobs.map(job => {
+                    if (appliedJobIds.includes(job._id)) {
+                        return {...job, alreadyApplied: true};
+                    }
+                    return job;
+                });
+                setallAvailableJobs(updatedJobs);
+                console.log(updatedJobs);
             })
-            setAppliedJobs([...appliedJobs, ...tempList])
         }
-        
-        
-    },[])
+    }, [isStudent, allJobsFetched])
 
-    const [appliedJobs, setAppliedJobs] = useState([])
-    const handleApply = (jobID) => {
-        setAppliedJobs([...appliedJobs, jobID])
-        // console.log("handleApply -- ",jobId)
+    const handleApply = (job_id) => {
+        const updatedJobs = allAvailableJobs.map(job => {
+            if (job_id == job._id) {
+                return {...job, alreadyApplied: true};
+            }
+            return job;
+        });
+        setallAvailableJobs(updatedJobs);
         
-        const candidateID = localStorage.getItem('userid');
-        const url = `candidate/${candidateID}/apply/${jobID}`
+        const candidate_id = localStorage.getItem('userid');
+        const url = `/candidate/${candidate_id}/apply/${job_id}`
         ApiFun.postApi(url).then((e) => {
             console.log(e);
         }).catch((err)=>{
             console.log(err);
         })
-        
-    }
-
-    const isInAppliedJobsList = (jobID) => {
-        // console.log(appliedJobs);
-        return appliedJobs.find(job => job === jobID)
     }
 
     return (
-    <h1>
-        {allAvailableJobs.map(((job) => (
-            <Card className='card'
-            sx={{
-                boxShadow: 1,
-                borderRadius: 2,
-                
-                marginTop: 5,
-                marginLeft: 5,
-                
-                width: 1000,
-                height: 150
-                }}
-                key={job._id}
-                >
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Company Name: {job.companyName}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Job Title: {job.jobTitle}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Job Description: {job.jobDescription}
-                    </Typography>
-                    {/* <Typography gutterBottom variant="h5" component="div">
-                        Location: {job.location}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Relatice Skills: {job.skillSets}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {job.description}
-                    </Typography> */}
-                </CardContent>
-                {isStudent && (
-                    <Button variant="contained" 
-                            color={isInAppliedJobsList(job._id) ? "secondary" : "success" }
-                            onClick={() => handleApply(job._id)}>
-                        {isInAppliedJobsList(job._id) ? "Applied" : "Apply" }
-                    </Button>
-                )}
-            </Card>
-        )))}
-    </h1>
+        <div>
+        <h1>
+            {allAvailableJobs.map(((job) => (
+                <Card className='card'
+                sx={{
+                    boxShadow: 1,
+                    borderRadius: 2,
+                    
+                    marginTop: 5,
+                    marginLeft: 5,
+                    
+                    width: 1000,
+                    height: 150
+                    }}
+                    key={job._id}
+                    >
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Company Name: {job.companyName}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Job Title: {job.jobTitle}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Job Description: {job.jobDescription}
+                        </Typography>
+                    </CardContent>
+                    {isStudent && (
+                        <Button variant="contained" 
+                                color={job.alreadyApplied ? "secondary" : "success" }
+                                onClick={() => handleApply(job._id)}>
+                            {job.alreadyApplied ? "Applied" : "Apply" }
+                        </Button>
+                    )}
+                </Card>
+            )))}
+        </h1>
+    </div>
     );
 }
