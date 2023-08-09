3811e0a6fc0d880776594711bdc43023a5ade2fc
Alor-e <42614595+Alor-e@users.noreply.github.com>
2023-08-08 23:19:25 -0400

    feat(tests): mock tests

--- be-career-platform/tests/conftest.py
+++ be-career-platform/tests/conftest.py
@@ -5,7 +5,7 @@ from server.model import User
 from server.extensions import mongo, bcrypt
 from dotenv import load_dotenv
 
-# Load the environment variables from the .env file
+# # Load the environment variables from the .env file
 load_dotenv()
 
 @pytest.fixture
@@ -22,32 +22,32 @@ def app():
 def client(app):
     return app.test_client()
 
-@pytest.fixture
-def init_database():
+# @pytest.fixture
+# def init_database():
 
-    password = "testuser"
-    password = bcrypt.generate_password_hash(password).decode('utf-8')
-    # Create a mock user
-    dummy_id = "1234"
-    mock_user = User(
-        _id=dummy_id,
-        email="testuser@example.com",
-        password=password,
-        role="candidate"
-    )
+#     password = "testuser"
+#     password = bcrypt.generate_password_hash(password).decode('utf-8')
+#     # Create a mock user
+#     dummy_id = "1234"
+#     mock_user = User(
+#         _id=dummy_id,
+#         email="testuser@example.com",
+#         password=password,
+#         role="candidate"
+#     )
 
-    mock_user.save_to_mongo()
+#     mock_user.save_to_mongo()
 
-    yield
-    mongo.db.users.find_one_and_delete({"_id": dummy_id})
+#     yield
+#     mongo.db.users.find_one_and_delete({"_id": dummy_id})
 
-@pytest.fixture
-def logged_in_client(client, init_database):
-    with client:
-        # Log in the user
-        response = client.post('/login', json={
-            'email': 'testuser@example.com',
-            'password': 'testuser'
-        })
+# @pytest.fixture
+# def logged_in_client(client):
+#     with client:
+#         # Log in the user
+#         response = client.post('/login', json={
+#             'email': 'testuser@example.com',
+#             'password': 'testuser'
+#         })
 
-        return client
+#         return client
--- be-career-platform/tests/test_candidate.py
+++ be-career-platform/tests/test_candidate.py
@@ -1,9 +1,16 @@
 import json
-from unittest.mock import patch
-from server.model import Candidate, Resume, Application
+from unittest.mock import patch, MagicMock
+from server.model import Candidate, User, Resume
 
-def test_index(app, init_database, logged_in_client):
-    res = logged_in_client.get('/candidate/index')
+@patch('server.model.User.get_by_id')
+def test_index(mock_get_by_id, app, client):
+    # Create a mock user
+    mock_user = User(email="test@mail.com", password="test1234", role="candidate", _id="1234")
+    
+    # When get_by_id is called, return the mock user
+    mock_get_by_id.return_value = mock_user
+
+    res = client.get('/candidate/index')
     assert res.status_code == 200
     assert "in candidate index" in res.get_data(as_text=True)
 
@@ -16,7 +23,7 @@ def test_get_profile(mock_get_by_id, app, client):
     assert "test@mail.com" in res.get_data(as_text=True)
 
 @patch('server.model.Candidate.save_to_mongo')
-def test_create_profile(mock_save_to_mongo, app, client):
+def test_create_profile(mock_save_to_mongo, client):
     mock_save_to_mongo.return_value = None
     data = {
         "email": "test@mail.com",
@@ -29,84 +36,61 @@ def test_create_profile(mock_save_to_mongo, app, client):
     assert res.status_code == 201
     assert "test@mail.com" in res.get_data(as_text=True)
 
-def test_update_profile(app, logged_in_client):
-    # Create a Candidate object and save it to your test database
+@patch('server.model.Candidate.get_by_id')
+@patch('server.model.Candidate.save_to_mongo')
+@patch('server.extensions.mongo.db')
+def test_update_profile(mock_db, mock_save_to_mongo, mock_get_by_id, client):
+    # Create a Candidate object
     candidate = Candidate(email="test@mail.com", password="test1234", role="candidate")
-    candidate.save_to_mongo()  # Save the candidate to generate an ID
-
-    with patch('server.model.Candidate.get_by_id') as mock_get_by_id, \
-            patch('server.extensions.mongo.db.candidate.find_one_and_update') as mock_find_one_and_update:
-
-        # Mock the get_by_id method to return the candidate you created
-        mock_get_by_id.return_value = candidate
-
-        # Define the updated document
-        updated_document = {
-            "_id": candidate._id,
-            "email": "updated@mail.com",
-            "password": "test1234",
-            "role": "candidate",
-        }
-
-        # Mock the find_one_and_update method to return the updated document
-        mock_find_one_and_update.return_value = updated_document
-
-        # Define the data to send in the request
-        updated_data = {
-            "email": "updated@mail.com",
-            "password": "test1234",
-            "role": "candidate",
-            "_id": candidate._id  # Use the ID of the candidate you created
-        }
-
-        # Send the POST request
-        res = logged_in_client.post(f'/candidate/profile/{candidate._id}', data=json.dumps(updated_data), content_type='application/json')
-
-        # Assert the status code and response data
-        assert res.status_code == 200
-        assert "updated@mail.com" in res.get_data(as_text=True)
-
-
-@patch('server.model.Candidate.get_by_id')
-@patch('server.model.Candidate.apply_to_job')
-def test_apply(mock_apply_to_job, mock_get_by_id, app, client):
-    candidate = Candidate(email="test@mail.com", password="test1234", role="candidate", _id="1234")
-    mock_get_by_id.return_value = candidate
-    mock_apply_to_job.return_value = True
-
-    res = client.post('/candidate/1234/apply/5678')
-    assert res.status_code == 200
-    assert "You have successfully applied to the job." in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.get_by_id')
-@patch('server.model.JobPosting.get_jobListsBYEmployerId')
-def test_get_jobs_by_employer(mock_get_jobListsBYEmployerId, mock_get_by_id, app, client):
-    candidate = Candidate(email="test@mail.com", password="test1234", role="candidate", _id="1234")
-    mock_get_by_id.return_value = candidate
-    mock_get_jobListsBYEmployerId.return_value = [{"job_id": "5678", "employer_id": "1234"}]
-
-    res = client.get('/candidate/1234/employer/5678')
-    assert res.status_code == 200
-    assert "5678" in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.get_by_id')
-@patch('server.model.Candidate.get_applied_jobs')
-def test_get_applied_jobs(mock_get_applied_jobs, mock_get_by_id, app, client):
-    candidate = Candidate(email="test@mail.com", password="test1234", role="candidate", _id="1234")
-    mock_get_by_id.return_value = candidate
-    mock_get_applied_jobs.return_value = [{"job_id": "5678", "employer_id": "1234"}]
-
-    res = client.get('/candidate/1234/jobs')
-    assert res.status_code == 200
-    assert "5678" in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.get_by_id')
-@patch('server.model.Resume.save_to_mongo')
-def test_create_resume(mock_save_to_mongo, mock_get_by_id, app, client):
-    candidate = Candidate(email="test@mail.com", password="test1234", role="candidate", _id="1234")
-    mock_get_by_id.return_value = candidate
     mock_save_to_mongo.return_value = None
+    mock_get_by_id.return_value = candidate
 
+    # Define the updated document
+    updated_document = {
+        "_id": candidate._id,
+        "email": "updated@mail.com",
+        "password": "test1234",
+        "role": "candidate",
+    }
+
+    # Mock the find_one_and_update method of the 'candidate' collection
+    mock_db.candidate.find_one_and_update.return_value = updated_document
+
+    # Define the data to send in the request
+    updated_data = {
+        "email": "updated@mail.com",
+        "password": "test1234",
+        "role": "candidate",
+        "_id": candidate._id  # Use the ID of the candidate you created
+    }
+
+    # Send the POST request
+    res = client.post(f'/candidate/profile/{candidate._id}', data=json.dumps(updated_data), content_type='application/json')
+
+    # Assert the status code and response data
+    assert res.status_code == 200
+    assert "updated@mail.com" in res.get_data(as_text=True)
+
+@patch.object(Candidate, 'get_by_id')
+@patch.object(Resume, 'save_to_mongo')
+@patch('server.extensions.mongo.db')
+@patch('uuid.uuid4')
+def test_create_resume(mock_uuid4, mock_db, mock_save_to_mongo, mock_get_by_id, client):
+    mock_uuid4.return_value.hex = "1234"
+    # Create a mock instance of Candidate
+    candidate_mock = MagicMock()
+
+    # Set the return value of the json() method to the desired dictionary
+    candidate_mock.json.return_value = {
+        "first_name": "Test",
+        "last_name": "User",
+        "email": "test@mail.com"\
+    }
+
+    # Set the return value of the get_by_id method to the mock instance
+    mock_get_by_id.return_value = candidate_mock
+
+    # Define the resume data
     resume_data = {
         "first_name": "Test",
         "last_name": "User",
@@ -120,84 +104,21 @@ def test_create_resume(mock_save_to_mongo, mock_get_by_id, app, client):
         "candidate_id": "1000"
     }
 
-    res = client.post('/candidate/1234/resume', data=json.dumps(resume_data), content_type='application/json')
+    # Mocking methods and return values
+    mock_save_to_mongo.return_value = None
+    mock_db.resumes.find_one_and_update.return_value = resume_data
+
+    # Send the POST request
+    res = client.post('/candidate/1000/resume', data=json.dumps(resume_data), content_type='application/json')
+
+    # Assert the status code
     assert res.status_code == 201
-    assert "test@mail.com" in res.get_data(as_text=True)
 
-@patch('server.model.Resume.get_by_candidate_id')
-def test_get_resume(mock_get_by_candidate_id, app, client):
-    resume = {
-        "first_name": "Test",
-        "last_name": "User",
-        "email": "test@mail.com",
-        "phone": "1234567890",
-        "education": [{"school": "Test University", "degree": "BSc", "field_of_study": "Computer Science", "start_year": "2018", "end_year": "2022"}],
-        "skills": ["python", "java"],
-        "experience": [{"job_title": "Software Engineer", "company": "Test Company", "start_date": "2022-01-01", "end_date": "2023-01-01", "description": "Test job description"}],
-        "file": {"file_name": "resume.pdf", "file_type": "pdf", "file_size": "1MB"},
-        "_id": "1234",
-        "candidate_id": "1000"
-    }
-    mock_get_by_candidate_id.return_value = resume
+    # Get the response data
+    response_data = json.loads(res.get_data(as_text=True))
 
-    res = client.get('/candidate/1234/resume')
-    assert res.status_code == 200
-    assert "test@mail.com" in res.get_data(as_text=True)
-
-@patch('server.model.Resume.get_by_id')
-def test_get_resume_by_id(mock_get_by_id, app, client):
-    resume = {
-        "first_name": "Test",
-        "last_name": "User",
-        "email": "test@mail.com",
-        "phone": "1234567890",
-        "education": [{"school": "Test University", "degree": "BSc", "field_of_study": "Computer Science", "start_year": "2018", "end_year": "2022"}],
-        "skills": ["python", "java"],
-        "experience": [{"job_title": "Software Engineer", "company": "Test Company", "start_date": "2022-01-01", "end_date": "2023-01-01", "description": "Test job description"}],
-        "file": {"file_name": "resume.pdf", "file_type": "pdf", "file_size": "1MB"},
-        "_id": "1234",
-        "candidate_id": "1000"
-    }
-    mock_get_by_id.return_value = resume
-
-    res = client.get('/candidate/resume/1234')
-    assert res.status_code == 200
-    assert "test@mail.com" in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.get_applications_by_candidate')
-def test_get_candidate_applications(mock_get_applications_by_candidate, app, client):
-    applications = [
-        {"_id": "1234", "candidate_id": "1000", "job_id": "5678", "status": "pending", "application_date": "2023-01-01"},
-        {"_id": "2345", "candidate_id": "1000", "job_id": "6789", "status": "accepted", "application_date": "2023-01-02"}
-    ]
-    mock_get_applications_by_candidate.return_value = applications
-
-    res = client.get('/candidate/1000/applications')
-    assert res.status_code == 200
-    assert "5678" in res.get_data(as_text=True)
-    assert "6789" in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.get_application')
-def test_get_candidate_application(mock_get_application, app, client):
-    application = {"_id": "1234", "candidate_id": "1000", "job_id": "5678", "status": "pending", "application_date": "2023-01-01"}
-    mock_get_application.return_value = application
-
-    res = client.get('/candidate/applications/1234')
-    assert res.status_code == 200
-    assert "5678" in res.get_data(as_text=True)
-
-@patch('server.model.Application.delete')
-def test_delete_candidate_application(mock_delete, app, client):
-    mock_delete.return_value = "Deleted document with ID: 1234"
-
-    res = client.delete('/candidate/applications/1234')
-    assert res.status_code == 200
-    assert "Deleted document with ID: 1234" in res.get_data(as_text=True)
-
-@patch('server.model.Candidate.delete')
-def test_delete_candidate(mock_delete, app, client):
-    mock_delete.return_value = "Deleted document with ID: 1234"
-
-    res = client.delete('/candidate/1234')
-    assert res.status_code == 200
-    assert "Deleted document with ID: 1234" in res.get_data(as_text=True)
+    # Assert the response data
+    assert response_data["_id"] == "1234"
+    assert response_data["email"] == "test@mail.com"
+    assert response_data["first_name"] == "Test"
+    assert response_data["last_name"] == "User"
